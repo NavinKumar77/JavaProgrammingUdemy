5e7c851a67ad61612f20a857e04853fc1e681d38
NavinKumar77 <89631106+NavinKumar77@users.noreply.github.com>
2023-02-25 20:23:53 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        boolean gameover = true;
-        int score = 800;
-        int level = 5;
-        int bonus = 100;
-        if(score < 5000 && score > 1000){
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        }
-        else if(score < 1000){
-            System.out.println("Your score was less than 1000");
-        }
-        else{
-            System.out.println("Got Here");
-        }
+
+        int myvariable = 50;
+        myvariable++;
+        myvariable--;
+        System.out.println("This is a Test");
+        System.out.println("This is " + "Another " + "Still more");
     }
 }
