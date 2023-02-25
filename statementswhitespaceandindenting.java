d3fd5ddf457e2d147b7be64b1f32a6a1b3ba94cc
NavinKumar77 <89631106+NavinKumar77@users.noreply.github.com>
2023-02-25 20:23:18 +0530

    Create statementswhitespaceandindenting.java

---
+++ statementswhitespaceandindenting.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int myvariable = 50;
+        myvariable++;
+        myvariable--;
+        System.out.println("This is a Test");
+        System.out.println("This is " + "Another " + "Still more");
+    }
+}
