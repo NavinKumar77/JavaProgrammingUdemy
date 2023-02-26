b4939e95bcb1a780560512cace4888f72bac5de4
NavinKumar77 <89631106+NavinKumar77@users.noreply.github.com>
2023-02-26 09:23:12 +0530

    Add files via upload

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,35 +1,20 @@
 public class MainChallenge {
     public static void main(String[] args) {
-        boolean gameover = true;
-        int score = 800;
-        int level = 5;
-        int bonus = 100;
+
+        calculatescore(true,800,5,100);
+
+        calculatescore(true,10000,8,200);
+
+    }
+
+    public static void calculatescore(boolean gameover, int score,int level,int bonus){
+
         int finalscore = score;
         if (gameover) {
             finalscore += (level * bonus);
             finalscore += 1000;
             System.out.println("Your Score is " + finalscore);
         }
-//        boolean gameover1 = true;
-//        int score1 = 10000;
-//        int level1 = 8;
-//        int bonus1 = 200;
-//        int finalscore1 = score1;
-//        if(gameover1 == true){
-//            finalscore1 += (level * bonus1 );
-//            System.out.println("Your Score is " + finalscore1);
-//        }
-
-
-         score = 10000;
-         level= 8;
-         bonus = 200;
-         finalscore = score;
-        if(gameover){
-            finalscore += (level * bonus );
-            System.out.println("Your Score is " + finalscore);
-            }
-
     }
     }
 
