650b25a5b2ac589bac0bea0832d0e36480cc44ca
NavinKumar77 <89631106+NavinKumar77@users.noreply.github.com>
2023-02-26 11:31:33 +0530

    Add files via upload

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,20 +1,36 @@
 public class MainChallenge {
     public static void main(String[] args) {
+        int highscoreposition = calculatehighscoreposition(1500);
+        displayhighscoreposition("Tim",highscoreposition);
+        highscoreposition = calculatehighscoreposition(1000);
+        displayhighscoreposition("bob",highscoreposition);
+        highscoreposition = calculatehighscoreposition(500);
+        displayhighscoreposition("kim",highscoreposition);
+        highscoreposition = calculatehighscoreposition(100);
+        displayhighscoreposition("john",highscoreposition);
+        highscoreposition = calculatehighscoreposition(25);
+        displayhighscoreposition("harry",highscoreposition);
 
-        calculatescore(true,800,5,100);
-
-        calculatescore(true,10000,8,200);
 
     }
 
-    public static void calculatescore(boolean gameover, int score,int level,int bonus){
+        public static void displayhighscoreposition(String playername,int highscoreposition){
+        System.out.println(playername + " managed to get into the position " +
+                highscoreposition + " on the high score list");
+        }
+        public static int calculatehighscoreposition(int playerscore){
+        if(playerscore >= 1000){
+            return 1;
+        }
+        else if(playerscore >= 500 && playerscore < 1000){
+            return 2;
+        }
+        else if(playerscore >= 100 && playerscore < 500){
+            return 3;
+        }
+        else{
+            return 4;
+        }
 
-        int finalscore = score;
-        if (gameover) {
-            finalscore += (level * bonus);
-            finalscore += 1000;
-            System.out.println("Your Score is " + finalscore);
         }
     }
-    }
-
